446bcc0b12ed67e513790c8feced9963739a81a7
Shelumiel Ediriarachchi <61077989+shelu-edi@users.noreply.github.com>
2023-01-23 18:30:40 +0530

    1/23/23

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// Hero
+ScrollReveal().reveal('.hero__desc__head', {delay: 500});
+ScrollReveal().reveal('.hero__desc__para', { delay: 800 });
+ScrollReveal().reveal('.visit__btn', { delay:1100 });
+ScrollReveal().reveal('.hero__container__img', { delay:1400 });
+
+// Brand
+ScrollReveal().reveal('.brand__container__head', {delay: 500});
+ScrollReveal().reveal('.brand__card', { delay: 800 });
+
+
+// Delivery
+ScrollReveal().reveal('.delivery__info__head', {delay: 500});
+ScrollReveal().reveal('.delivery__info__para', { delay: 800 });
+ScrollReveal().reveal('.delivery__container__img', { delay:1100 });
